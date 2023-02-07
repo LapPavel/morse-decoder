beae8242193e9227b6bd7a1975f20954d697f2d8
lappavel <wizd@tut.by>
2023-02-07 20:58:03 +0300

    fix: fixed comments

--- src/index.js
+++ src/index.js
@@ -40,16 +40,16 @@ const MORSE_TABLE = {
 function decode(expr) {
     let arr = [];
     for (let i = 0; i < expr.length; i += 10){
-        arr.push(expr.slice(i, i +10));
+        arr.push(expr.slice(i, i + 10)); //Разбиваем строку на вложенные массивы по 10 символов
     }
     let result = arr
-    .map(el => MORSE_TABLE[el //Заменяем код на буквы
+    .map(el => MORSE_TABLE[el //Заменяем код по ключю на буквы
     .slice(el.indexOf('1')) //Обрезаем все '0' до первой '1'
     .match(/.{1,2}/g) //Разбиваем массив по 2-а элемента
     .map(el => el === '11' ? '-' : el === '10' ? '.' : null) //Заменяем значение на '-' или '.' (!пробел получит undefined)
-    .join('')]); // Собираем ключ для объекта
-    let word = result.map(el => el !== undefined ? el : ' '); // Заменяем undefined на ' '(пробел)
-    return word.join(''); //Возвращаем собранную строку
+    .join('')]); //Собираем ключ для объекта
+    let word = result.map(el => el !== undefined ? el : ' ').join(''); //Заменяем undefined на ' '(пробел) и собираем строку
+    return word; //Возвращаем строку
 }
 
 module.exports = {
